ad9fcdc8ab15bd3737bcb15684f6dd4c39949847
Zoe Cope <zoe.cope@hmcts.net>
2023-09-08 19:17:24 +0100

    yarning

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -7,8 +7,7 @@ export class AppInsights {
     if (config.get('appInsights.instrumentationKey')) {
       appInsights.setup(config.get('appInsights.instrumentationKey')).setSendLiveMetrics(true).start();
 
-      appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] =
-        'pre-portal';
+      appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'pre-portal';
       appInsights.defaultClient.trackTrace({
         message: 'App insights activated',
       });
