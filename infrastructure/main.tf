c25c3e337f7fe010d55afe444bfbb50644cd8015
Jason Paige <j@jasonpaige.co.uk>
2024-02-08 20:26:24 +0000

    Add a Redis instance and store access key in KV

---
+++ infrastructure/main.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_key_vault" "key_vault" {
+  name                = "${var.product}-hmctskv-${var.env}"
+  resource_group_name = "${var.product}-${var.env}"
+}
+
+module "pre-portal-redis6" {
+  source                        = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product                       = var.product
+  name                          = "${var.product}-${var.component}-${var.env}"
+  location                      = var.location
+  env                           = var.env
+  private_endpoint_enabled      = true
+  redis_version                 = "6"
+  business_area                 = "sds"
+  public_network_access_enabled = false
+  common_tags                   = var.common_tags
+  sku_name                      = var.sku_name
+  family                        = var.family
+  capacity                      = var.capacity
+
+}
+
+resource "azurerm_key_vault_secret" "redis6_access_key" {
+  name         = "redis6-access-key"
+  value        = module.pre-portal-redis6.access_key
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
