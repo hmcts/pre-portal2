fa435771334a1b186eca44690c3f1644614773ed
Jason Paige <j@jasonpaige.co.uk>
2024-02-20 14:03:10 +0000

    watch

--- .pnp.cjs
+++ .pnp.cjs
@@ -78,7 +78,7 @@ const RAW_RUNTIME_STATE =
           ["express", "npm:4.18.1"],\
           ["express-nunjucks", "npm:2.2.5"],\
           ["glob", "npm:8.0.3"],\
-          ["govuk-frontend", "npm:4.3.1"],\
+          ["govuk-frontend", "npm:5.1.0"],\
           ["helmet", "npm:6.0.0"],\
           ["html-webpack-plugin", "virtual:16701266ea8f9c4c47e5d4e7d6e5f60f5b90bd85760be31472205688a0f541b309f8b385bcdfaeae586863ad36065412b38f52d324d6841d21abacc4b09d6704#npm:5.5.0"],\
           ["husky", "npm:8.0.1"],\
@@ -11999,10 +11999,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["govuk-frontend", [\
-      ["npm:4.3.1", {\
-        "packageLocation": "../../../.yarn/berry/cache/govuk-frontend-npm-4.3.1-2ce4f02354-10c0.zip/node_modules/govuk-frontend/",\
+      ["npm:5.1.0", {\
+        "packageLocation": "../../../.yarn/berry/cache/govuk-frontend-npm-5.1.0-6932aa0d93-10c0.zip/node_modules/govuk-frontend/",\
         "packageDependencies": [\
-          ["govuk-frontend", "npm:4.3.1"]\
+          ["govuk-frontend", "npm:5.1.0"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -16357,7 +16357,7 @@ const RAW_RUNTIME_STATE =
           ["express", "npm:4.18.1"],\
           ["express-nunjucks", "npm:2.2.5"],\
           ["glob", "npm:8.0.3"],\
-          ["govuk-frontend", "npm:4.3.1"],\
+          ["govuk-frontend", "npm:5.1.0"],\
           ["helmet", "npm:6.0.0"],\
           ["html-webpack-plugin", "virtual:16701266ea8f9c4c47e5d4e7d6e5f60f5b90bd85760be31472205688a0f541b309f8b385bcdfaeae586863ad36065412b38f52d324d6841d21abacc4b09d6704#npm:5.5.0"],\
           ["husky", "npm:8.0.1"],\
