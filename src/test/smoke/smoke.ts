43bcbf50d205519ffde7da1c3d1fc0fc10ffa4e4
Jason Paige <j@jasonpaige.co.uk>
2024-02-21 14:45:25 +0000

    smoke test fix

--- src/test/smoke/smoke.ts
+++ src/test/smoke/smoke.ts
@@ -3,7 +3,7 @@ import { fail } from 'assert';
 import axios, { AxiosResponse } from 'axios';
 import { expect } from 'chai';
 
-const testUrl = process.env.TEST_URL || 'http://localhost:4550';
+const testUrl = process.env.TEST_URL || 'http://localhost:4550/sign-in';
 
 describe('Smoke Test', () => {
   describe('Home page loads', () => {
@@ -14,7 +14,7 @@ describe('Smoke Test', () => {
             'Accept-Encoding': 'gzip',
           },
         });
-        expect(response.data).includes('<app-root></app-root>');
+        expect(response.data).includes('<h1 class="govuk-heading-xl">Sign in</h1>');
       } catch {
         fail('Heading not present and/or correct');
       }
