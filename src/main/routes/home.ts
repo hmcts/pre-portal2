f9461f21f109a2ee65306f5c4ad468ae00df558d
Jason Paige <j@jasonpaige.co.uk>
2024-02-26 14:51:00 +0000

    tests

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -4,6 +4,5 @@ export default function (app: Application): void {
   app.get('/', (req, res) => {
     // check if user is signed in or not and redirect accordingly
     res.redirect('/browse');
-    // res.send('OIDC ' + JSON.stringify(req.oidc.user));
   });
 }
