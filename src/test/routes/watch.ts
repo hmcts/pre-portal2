e015729d7a713c3d5723f599500223c31cb3a3bd
Jason Paige <j@jasonpaige.co.uk>
2024-02-26 16:22:26 +0000

    mock PreClient in watch test

--- src/test/routes/watch.ts
+++ src/test/routes/watch.ts
@@ -2,6 +2,41 @@ import { app } from '../../main/app';
 
 import { expect } from 'chai';
 import request from 'supertest';
+import { Recording } from '../../main/services/pre-api/types';
+import { PreClient } from '../../main/services/pre-api/pre-client';
+
+jest.spyOn(PreClient.prototype, 'getRecording').mockImplementation(async (id: string) => {
+  if (id === 'something') {
+    return Promise.resolve({
+      id: 'something',
+      parentRecordingId: 'parentId',
+      version: 1,
+      filename: 'filename',
+      duration: 'duration',
+      editInstructions: 'editInstructions',
+      captureSession: {
+        id: '',
+        bookingId: '',
+        origin: '',
+        ingestAddress: '',
+        liveOutputUrl: '',
+        startedAt: '',
+        startedByUserId: '',
+        finishedAt: '',
+        finishedByUserId: '',
+        status: '',
+        deletedAt: '',
+        courtName: '',
+      },
+      deletedAt: 'deletedAt',
+      createdAt: 'createdAt',
+      caseReference: 'caseReference',
+      isTestCase: false,
+      participants: [],
+    } as Recording);
+  }
+  return Promise.resolve(null);
+});
 
 /* eslint-disable jest/expect-expect */
 describe('Watch page', () => {
