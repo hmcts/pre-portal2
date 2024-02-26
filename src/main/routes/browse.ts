e9afd976d7a5d041f8291e97ba6f7a2d1d528011
Jason Paige <j@jasonpaige.co.uk>
2024-02-26 09:42:30 +0000

    Login ok

--- src/main/routes/browse.ts
+++ src/main/routes/browse.ts
@@ -2,8 +2,6 @@ import { Application } from 'express';
 
 export default function (app: Application): void {
   app.get('/browse', async (req, res) => {
-    // const userInfo = await req.oidc.fetchUserInfo();
-    // res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
     // having to check if oidc is defined is a bit of a code smell but needed whilst pa11y is testing when logged out
     res.render('browse', { user: req.oidc?.user });
   });
