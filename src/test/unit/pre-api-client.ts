3e8331b30a67419bbf2cd9704eda4c61143a6924
Laurence Rawlings <laurence.rawlings@hmcts.net>
2024-02-29 09:57:08 +0000

    fix: coverage

--- src/test/unit/pre-api-client.ts
+++ src/test/unit/pre-api-client.ts
@@ -1,38 +1,64 @@
-import { mock, reset } from '../mock-api';
+import { mockRecordings } from '../mock-api';
 import { PreClient } from '../../main/services/pre-api/pre-client';
 import { SearchRecordingsRequest } from '../../main/services/pre-api/types';
 import { describe } from '@jest/globals';
+import axios from 'axios';
 
 const preClient = new PreClient();
 
 /* eslint-disable jest/expect-expect */
 describe('PreClient success', () => {
   test('get recording', async () => {
-    mock();
+    jest.spyOn(axios, 'get').mockResolvedValue({ data: mockRecordings[0] } as any);
     const recording = await preClient.getRecording('something');
     expect(recording).toBeTruthy();
   });
   test('get recordings', async () => {
-    mock();
+    jest.spyOn(axios, 'get').mockResolvedValue({ data: { _embedded: { recordingDTOList: mockRecordings } } } as any);
     const request = {} as SearchRecordingsRequest;
     const recordings = await preClient.getRecordings(request);
     expect(recordings).toBeTruthy();
+  });
+  test('get recordings when none are found', async () => {
+    jest.spyOn(axios, 'get').mockResolvedValue(null as any);
+    const request = {} as SearchRecordingsRequest;
+    const recordings = await preClient.getRecordings(request);
+    expect(recordings).toEqual([]);
+  });
+  test('get recording playback data', async () => {
+    jest.spyOn(axios, 'create').mockReturnValue(axios);
+    jest.spyOn(axios, 'post').mockResolvedValue({
+      data: {
+        manifestpath: 'manifestpath',
+        aestoken: 'aestoken',
+      },
+    } as any);
+    const recordingPlaybackData = await preClient.getRecordingPlaybackData('something');
+    expect(recordingPlaybackData).toBeTruthy();
   });
 });
 
 /* eslint-disable jest/expect-expect */
 describe('PreClient failure', () => {
   test('get recording', async () => {
+    jest.spyOn(axios, 'get').mockResolvedValue({ data: null } as any);
     const recording = await preClient.getRecording('something');
     expect(recording).toBeFalsy();
   });
   test('get recordings', async () => {
+    jest.spyOn(axios, 'get').mockImplementation(() => {
+      throw new Error('error');
+    });
     const request = {} as SearchRecordingsRequest;
     const recordings = await preClient.getRecordings(request);
     expect(recordings).toBeFalsy();
   });
-});
-
-beforeEach(() => {
-  reset();
+  test('get recording playback data', async () => {
+    jest.spyOn(axios, 'create').mockReturnValue(axios);
+    jest.spyOn(axios, 'post').mockImplementation(() => {
+      throw new Error('error');
+    });
+    const recordingPlaybackData = await preClient.getRecordingPlaybackData('something');
+    expect(recordingPlaybackData).toBeFalsy();
+  });
 });
