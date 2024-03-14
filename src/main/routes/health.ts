8b58ae6ce1ef33dc54de9c4f39e4844d7e27e385
Jason Paige <j@jasonpaige.co.uk>
2024-03-14 11:30:24 +0000

    Proper healthcheck

--- src/main/routes/health.ts
+++ src/main/routes/health.ts
@@ -1,14 +1,32 @@
+import os from 'os';
+
+import healthcheck from '@hmcts/nodejs-healthcheck';
+import config from 'config';
 import { Application } from 'express';
 
-const healthcheck = require('@hmcts/nodejs-healthcheck');
-
 export default function (app: Application): void {
-  const healthCheckConfig = {
-    checks: {
-      // TODO: replace this sample check with proper checks for your application
-      sampleCheck: healthcheck.raw(() => healthcheck.up()),
-    },
-  };
+  const redis = app.locals.redisClient
+    ? healthcheck.raw(() => app.locals.redisClient.ping().then(healthcheck.up).catch(healthcheck.down))
+    : null;
 
-  healthcheck.addTo(app, healthCheckConfig);
+  healthcheck.addTo(app, {
+    checks: {
+      // currently no API health check is possible for B2C
+      ...(redis ? { redis } : {}),
+      'pre-api': healthcheck.web(new URL('/health', config.get('pre.apiUrl'))),
+    },
+    ...(redis
+      ? {
+          readinessChecks: {
+            // allow the frontend to start up without the API but not without redis.
+            redis,
+          },
+        }
+      : {}),
+    buildInfo: {
+      name: 'pre-portal',
+      host: os.hostname(),
+      uptime: process.uptime(),
+    },
+  });
 }
