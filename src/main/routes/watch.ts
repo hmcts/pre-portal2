4446e987e3655ba92c0b41749d65cd7970de7229
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-04-15 12:02:54 +0100

    Add call to put audit from watch page

--- src/main/routes/watch.ts
+++ src/main/routes/watch.ts
@@ -3,6 +3,7 @@ import { SessionUser } from '../services/session-user/session-user';
 
 import { Application } from 'express';
 import { requiresAuth } from 'express-openid-connect';
+import { v4 as uuid } from 'uuid';
 
 function validateId(id: string): boolean {
   return /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(id);
@@ -45,9 +46,10 @@ export default function (app: Application): void {
 
     try {
       const client = new PreClient();
+      const userPortalId = await SessionUser.getLoggedInUserPortalId(req);
 
       const recordingPlaybackData = await client.getRecordingPlaybackData(
-        await SessionUser.getLoggedInUserPortalId(req),
+        userPortalId,
         req.params.id
       );
 
@@ -56,6 +58,20 @@ export default function (app: Application): void {
         res.json({ message: 'Not found' });
         return;
       }
+
+      await client.putAudit(
+        userPortalId,
+        {
+          id: uuid(),
+          functional_area: 'Video Player',
+          category: 'Recording',
+          activity: 'Play',
+          source: 'PORTAL',
+          audit_details: {
+            recordingId: req.params.id,
+          }
+        }
+      );
 
       res.json(recordingPlaybackData);
     } catch (e) {
