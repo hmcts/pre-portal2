36873a1767e2367ca101724c1a78b43dd7723c8f
Jason Paige <j@jasonpaige.co.uk>
2024-04-22 12:22:36 +0100

    Move pa11y to after FTs

--- src/test/a11y/a11y.ts
+++ src/test/a11y/a11y.ts
@@ -1,54 +1,21 @@
-import { app } from '../../main/app';
-
-import request from 'supertest';
-import { afterAll } from '@jest/globals';
-import { mock, reset } from '../mock-api';
+import puppeteer, { Browser } from 'puppeteer';
+import { config } from '../config';
 
 const pa11y = require('pa11y');
 
-const appServers: any[] = [];
-function server() {
-  const server = app.listen();
-  appServers.push(server);
-  return server;
-}
-
-class Pa11yResult {
+interface Pa11yResult {
   documentTitle: string;
   pageUrl: string;
   issues: PallyIssue[];
-
-  constructor(documentTitle: string, pageUrl: string, issues: PallyIssue[]) {
-    this.documentTitle = documentTitle;
-    this.pageUrl = pageUrl;
-    this.issues = issues;
-  }
 }
 
-class PallyIssue {
+interface PallyIssue {
   code: string;
   context: string;
   message: string;
   selector: string;
   type: string;
   typeCode: number;
-
-  constructor(code: string, context: string, message: string, selector: string, type: string, typeCode: number) {
-    this.code = code;
-    this.context = context;
-    this.message = message;
-    this.selector = selector;
-    this.type = type;
-    this.typeCode = typeCode;
-  }
-}
-
-function runPally(url: string): Promise<Pa11yResult> {
-  return pa11y(url, {
-    hideElements: '.govuk-footer__licence-logo, .govuk-header__logotype-crown',
-    // Ignoring NoSuchID due to how Angular app links between pages: Links say they are linking to '/browse' when they actually link to '/#/browse' which is not listed as a route.
-    ignore: ['WCAG2AA.Principle2.Guideline2_4.2_4_1.G1,G123,G124.NoSuchID'],
-  });
 }
 
 function expectNoErrors(messages: PallyIssue[]): void {
@@ -60,41 +27,76 @@ function expectNoErrors(messages: PallyIssue[]): void {
   }
 }
 
-function testAccessibility(url: string): void {
-  describe(`Page ${url}`, () => {
+jest.setTimeout(5000);
+describe('Accessibility', () => {
+  let browser: Browser;
+  let hasAfterAllRun = false;
+
+  const setup = async () => {
+    if (hasAfterAllRun) {
+      return;
+    }
+    if (browser) {
+      await browser.close();
+    }
+    browser = await puppeteer.launch({ ignoreHTTPSErrors: true });
+  };
+
+  const signedOutUrls = ['/terms-and-conditions', '/accessibility-statement', '/cookies', '/not-found', '/'];
+
+  beforeAll(setup);
+
+  afterAll(async () => {
+    hasAfterAllRun = true;
+    await browser.close();
+  });
+
+  describe.each(signedOutUrls)('Signed out page %s', url => {
     test('should have no accessibility errors', async () => {
-      // await ensurePageCallWillSucceed(url);
-      const result = await runPally(await request(server()).get(url).url);
+      const result: Pa11yResult = await pa11y(config.TEST_URL + url, {
+        screenCapture: `functional-output/pa11y/${url}.png`,
+        browser: browser,
+      });
       expect(result.issues).toEqual(expect.any(Array));
       expectNoErrors(result.issues);
     });
   });
-}
 
-jest.mock('express-openid-connect', () => {
-  return {
-    requiresAuth: jest.fn().mockImplementation(() => {
-      return (req: any, res: any, next: any) => {
-        next();
-      };
-    }),
-  };
+  test('/browse page', async () => {
+    console.log(config.TEST_URL);
+    const testUrl = config.TEST_URL as string;
+    const page = await browser.newPage();
+    await page.goto(testUrl);
+    await page.waitForSelector('#signInName', { visible: true, timeout: 0 });
+    await page.type('#signInName', process.env.B2C_TEST_LOGIN_EMAIL as string);
+    await page.type('#password', process.env.B2C_TEST_LOGIN_PASSWORD as string);
+    await page.click('#next');
+    // const cookies = await page.cookies(testUrl);
+    await page.close();
+
+    const result: Pa11yResult = await pa11y(testUrl + '/browse', {
+      browser: browser,
+      screenCapture: `functional-output/pa11y/browse.png`,
+    });
+    expect(result.issues).toEqual(expect.any(Array));
+    expectNoErrors(result.issues);
+
+    // const browsePage = await browser.newPage();
+    // await browsePage.goto(testUrl);
+    // await browsePage.waitForSelector('a[href^=/watch/]', { visible: true, timeout: 0 });
+    // await browsePage.click('a[href^=/watch/]');
+    // const watchUrl = browsePage.url();
+    // await browsePage.close();
+    //
+    // const watchResult: Pa11yResult = await pa11y(watchUrl, {
+    //   browser: browser,
+    //   screenCapture: `functional-output/pa11y/watch.png`
+    // });
+    // expect(watchResult.issues).toEqual(expect.any(Array));
+    // expectNoErrors(watchResult.issues);
+  }, 30000);
 });
 
-jest.setTimeout(30000);
-mock();
-
-describe('Accessibility', () => {
-  afterAll(async () => {
-    appServers.forEach(server => server.close());
-    reset();
-  });
-  // testing accessibility of the home page
-  testAccessibility('/terms-and-conditions');
-  testAccessibility('/accessibility-statement');
-  testAccessibility('/sign-in');
-  testAccessibility('/browse');
-  testAccessibility('/not-found');
-  testAccessibility('/watch/something');
-  testAccessibility('/cookies');
-});
+// testing accessibility of the home page
+// testAccessibility('/browse');
+// testAccessibility('/watch/something');
