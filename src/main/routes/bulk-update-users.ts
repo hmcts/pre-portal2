5537241ff7c08cd3fc68711796cf1cbb5532f0b3
Marianne Azzopardi <76755256+marazzo@users.noreply.github.com>
2024-05-29 15:18:40 +0100

    adding in logic to parse data csv

--- src/main/routes/bulk-update-users.ts
+++ src/main/routes/bulk-update-users.ts
@@ -3,25 +3,44 @@ import { PreClient } from '../services/pre-api/pre-client';
 import { Logger } from '@hmcts/nodejs-logging';
 import { Application } from 'express';
 
+import * as fs from "fs";
+import * as path from "path";
+import csvParser from 'csv-parser';
+
 export default function (app: Application): void {
   app.get('/bulk-update-users', () => {
     const logger = Logger.getLogger('bulk-update-users');
     const client = new PreClient();
-    const xUserId = 'da940fcc-380d-4209-b385-31a76311975a';
-    const usersToUpdate = [{ from: 'from@somewhere.com', to: 'somwhere@else.net' }];
+    // const xUserId = 'da940fcc-380d-4209-b385-31a76311975a';
+    // const usersToUpdate = [{ from: 'from@somewhere.com', to: 'somwhere@else.net' }];
+    const csvFilePath = path.resolve('__dirname', '');
+    const usersToUpdate: { from: string; to: string }[] = [];
 
-    usersToUpdate.forEach(async user => {
-      try {
-        logger.info(`Updating user ${user.from} to ${user.to}`);
-        if (await client.updateUser(user.from, user.to, xUserId)) {
-          logger.info(`User ${user.from} successfully updated to ${user.to}`);
-        } else {
-          logger.warn(`User ${user.from} failed to update to ${user.to}`);
-        }
-      } catch (e) {
-        logger.error(e.message());
-        logger.warn(`User ${user.from} failed to update to ${user.to}`);
-      }
-    });
+    fs.createReadStream(csvFilePath)
+      .pipe(csvParser())
+      .on('data', (row) => {
+        usersToUpdate.push({ from: row['email'], to: row['migrated email'] });
+      })
+      .on('end', async () => {
+        logger.info('CSV file successfully processed');
+        
+        usersToUpdate.forEach(async user => {
+          try {
+            logger.info(`Fetching user_id for user ${user.from}`);
+            const userProfile = await client.getUserByEmail(user.from);
+            const xUserId = userProfile.user.id;
+
+            logger.info(`Updating user ${user.from} to ${user.to}`);
+            if (await client.updateUser(user.from, user.to, xUserId)) {
+              logger.info(`User ${user.from} successfully updated to ${user.to}`);
+            } else {
+              logger.warn(`User ${user.from} failed to update to ${user.to}`);
+            }
+          } catch (e) {
+            logger.error(e.message());
+            logger.warn(`User ${user.from} failed to update to ${user.to}`);
+          }
+        });
+      })
   });
 }
