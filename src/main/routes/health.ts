a15fac9befd532eab9de54796d86e3cf48e0fa0d
Jason Paige <j@jasonpaige.co.uk>
2024-08-23 15:03:29 +0100

    remove redis from health check

--- src/main/routes/health.ts
+++ src/main/routes/health.ts
@@ -3,30 +3,26 @@ import os from 'os';
 import { PreClient } from '../services/pre-api/pre-client';
 
 import healthcheck from '@hmcts/nodejs-healthcheck';
+import { Logger } from '@hmcts/nodejs-logging';
 import { Application } from 'express';
 
 export default function (app: Application): void {
-  const redis = app.locals.redisClient
-    ? healthcheck.raw(() => app.locals.redisClient.ping().then(healthcheck.up).catch(healthcheck.down))
-    : null;
+  // const redis = app.locals.redisClient
+  //   ? healthcheck.raw(() => app.locals.redisClient.ping().then(healthcheck.up).catch(healthcheck.down))
+  //   : null;
+  if (app.locals.redisClient) {
+    const logger = Logger.getLogger('health');
+    logger.info('redis ping result: ' + app.locals.redisClient.ping());
+  }
 
   healthcheck.addTo(app, {
     checks: {
       // currently no API health check is possible for B2C
-      ...(redis ? { redis } : {}),
       'pre-api': healthcheck.raw(() => {
         const client = new PreClient();
         return client.healthCheck().then(healthcheck.up).catch(healthcheck.down);
       }),
     },
-    ...(redis
-      ? {
-          readinessChecks: {
-            // allow the frontend to start up without the API but not without redis.
-            redis,
-          },
-        }
-      : {}),
     buildInfo: {
       name: 'pre-portal',
       host: os.hostname(),
