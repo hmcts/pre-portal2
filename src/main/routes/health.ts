7f834e6fc9d59c49f1f3cb59c4e83730f2e468a1
Jason Paige <j@jasonpaige.co.uk>
2024-08-23 19:36:43 +0100

    just healthcheck redis

--- src/main/routes/health.ts
+++ src/main/routes/health.ts
@@ -3,9 +3,24 @@ import os from 'os';
 import healthcheck from '@hmcts/nodejs-healthcheck';
 import { Application } from 'express';
 
-export default async function (app: Application): Promise<void> {
+export default function (app: Application): void {
+  const redis = app.locals.redisClient
+    ? healthcheck.raw(() => app.locals.redisClient.ping().then(healthcheck.up).catch(healthcheck.down))
+    : null;
+
   healthcheck.addTo(app, {
-    checks: {},
+    checks: {
+      // currently no API health check is possible for B2C
+      ...(redis ? { redis } : {}),
+    },
+    ...(redis
+      ? {
+          readinessChecks: {
+            // allow the frontend to start up without the API but not without redis.
+            redis,
+          },
+        }
+      : {}),
     buildInfo: {
       name: 'pre-portal',
       host: os.hostname(),
