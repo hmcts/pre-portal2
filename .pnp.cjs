ecdd812464e1329264798e5df498690c66818ba4
Jason Paige <j@jasonpaige.co.uk>
2024-09-18 17:27:33 +0100

    --ext fix

--- .pnp.cjs
+++ .pnp.cjs
@@ -32,6 +32,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@babel/preset-env", "virtual:16701266ea8f9c4c47e5d4e7d6e5f60f5b90bd85760be31472205688a0f541b309f8b385bcdfaeae586863ad36065412b38f52d324d6841d21abacc4b09d6704#npm:7.25.4"],\
             ["@codeceptjs/allure-legacy", "npm:1.0.2"],\
             ["@codeceptjs/configure", "virtual:3be9c99fb09831d56b48191ca70d8581f26f5c353d7e5239ff306fcae05ab00718cfa696542446ba1c52253c4408c82071683a1888658dcefd905422852eb2c8#npm:1.0.1"],\
+            ["@eslint/compat", "npm:1.1.1"],\
             ["@hmcts/cookie-manager", "npm:1.0.0"],\
             ["@hmcts/info-provider", "npm:1.2.1"],\
             ["@hmcts/nodejs-healthcheck", "npm:1.8.5"],\
@@ -3706,6 +3707,15 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/@eslint-community-regexpp-npm-4.11.1-37bbb67aaa-6986685529.zip/node_modules/@eslint-community/regexpp/",\
           "packageDependencies": [\
             ["@eslint-community/regexpp", "npm:4.11.1"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["@eslint/compat", [\
+        ["npm:1.1.1", {\
+          "packageLocation": "./.yarn/cache/@eslint-compat-npm-1.1.1-2a9781ae21-c9146b139e.zip/node_modules/@eslint/compat/",\
+          "packageDependencies": [\
+            ["@eslint/compat", "npm:1.1.1"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -19053,6 +19063,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@babel/preset-env", "virtual:16701266ea8f9c4c47e5d4e7d6e5f60f5b90bd85760be31472205688a0f541b309f8b385bcdfaeae586863ad36065412b38f52d324d6841d21abacc4b09d6704#npm:7.25.4"],\
             ["@codeceptjs/allure-legacy", "npm:1.0.2"],\
             ["@codeceptjs/configure", "virtual:3be9c99fb09831d56b48191ca70d8581f26f5c353d7e5239ff306fcae05ab00718cfa696542446ba1c52253c4408c82071683a1888658dcefd905422852eb2c8#npm:1.0.1"],\
+            ["@eslint/compat", "npm:1.1.1"],\
             ["@hmcts/cookie-manager", "npm:1.0.0"],\
             ["@hmcts/info-provider", "npm:1.2.1"],\
             ["@hmcts/nodejs-healthcheck", "npm:1.8.5"],\
