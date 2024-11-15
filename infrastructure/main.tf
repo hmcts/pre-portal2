7f3ceaa4206e7fbe251594b6eb3c0d65f3124552
damongreen123 <damon.green@btinternet.com>
2024-11-15 16:22:39 +0000

    Test do not merge Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,7 +18,7 @@ module "pre-portal-redis6" {
   business_area                 = "sds"
   public_network_access_enabled = false
   common_tags                   = var.common_tags
-  sku_name                      = var.sku_name
+  sku_name                      = Basic
   family                        = var.family
   capacity                      = var.capacity
 
