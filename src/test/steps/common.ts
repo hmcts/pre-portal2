0b0833e9513710400a9ee770bc2c3a24056f05e5
satyachundur <>
2024-12-09 16:10:01 +0000

    adding playback test scenario

--- src/test/steps/common.ts
+++ src/test/steps/common.ts
@@ -20,7 +20,7 @@ Then('the page title should include {string}', (text: string) => {
   I.seeInTitle(text);
 });
 
-Then('I am on the {string} page', (path: string) => {
+When('I am on the {string} page', (path: string) => {
   const url = new URL(path, config.TEST_URL);
   I.amOnPage(url.toString());
 });
@@ -81,3 +81,37 @@ Then('I sign in with the wrong password', () => {
     }
   });
 });
+
+  When('I click on play on a browse page', () => {
+    I.click('Play');
+    I.see('Please note playback is preferred on non-mobile devices. If possible, please use a preferred device');
+  });
+
+  When('I play the recording', () => {
+    I.wait(5); //needed as it takes time to load recording on page.
+    I.waitForElement('//*[@aria-label=\'Play/Pause\']')
+    I.click('Play/Pause');
+  });
+
+  Then('recording is played', async () => {
+    I.wait(15); //waiting for mediakind to generate streaming locator.
+    const videoSelector = '.bmpui-ui-playbacktimelabel'
+    try {
+      I.waitForElement(videoSelector, 5); // Fail the test if the element does not appear
+      I.say('Playback time label is visible.');
+    } catch (error) {
+      throw new Error('Playback time label does not exist or is not visible.');
+    }
+    const initialTime = await I.grabTextFrom('.bmpui-ui-playbacktimelabel:nth-of-type(1)');
+    I.wait(5)
+    I.click('Play/Pause');
+    const currentTime = await I.grabTextFrom('.bmpui-ui-playbacktimelabel:nth-of-type(2)');
+    if (!currentTime.match(/^\d{2}:\d{2}$/)) {
+      throw new Error(`Invalid playback time format: ${currentTime}`);
+    }
+
+    if (currentTime <= initialTime) {
+      throw new Error('Video is not playing');
+    }
+    I.say('Video is playing successfully!');
+  });
