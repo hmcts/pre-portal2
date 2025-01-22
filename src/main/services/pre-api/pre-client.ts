7de5420084e46d511f9bfacff4b22a87ec5f5ae4
Phoebe Revolta <phoebe.revolta@solirius.com>
2025-01-22 15:02:54 +0000

    S28-3557: refactor pagination on browse & audit pages into method

--- src/main/services/pre-api/pre-client.ts
+++ src/main/services/pre-api/pre-client.ts
@@ -19,6 +19,90 @@ import config from 'config';
 export class PreClient {
   logger = Logger.getLogger('pre-client');
 
+  public createPagination(
+    pagination: Pagination,
+    url: string,
+    pageTitle: string,
+    resourceLength: number
+  ): {
+    paginationLinks: {
+      previous: {};
+      next: {};
+      items: ({ href: string; number: number; current: boolean } | { ellipsis: boolean })[];
+    };
+    title: string;
+  } {
+    const paginationLinks = {
+      previous: {},
+      next: {},
+      items: [] as ({ href: string; number: number; current: boolean } | { ellipsis: boolean })[],
+    };
+
+    // Add previous link if not on the first page
+    if (pagination.currentPage > 0) {
+      paginationLinks.previous = {
+        href: `/` + url + `?page=${pagination.currentPage - 1}`,
+      };
+    }
+
+    // Add next link if not on the last page
+    if (pagination.currentPage < pagination.totalPages - 1) {
+      paginationLinks.next = {
+        href: `/` + url + `?page=${pagination.currentPage + 1}`,
+      };
+    }
+
+    // Always add the first page
+    paginationLinks.items.push({
+      href: '/' + url + '?page=0',
+      number: 1,
+      current: 0 === pagination.currentPage,
+    });
+
+    // Add an ellipsis after the first page if the 2nd page is not in the window
+    if (pagination.currentPage > 3) {
+      paginationLinks.items.push({ ellipsis: true });
+    }
+
+    // Add the pages immediately 2 before and 2 after the current page to create a rolling window of 5 pages
+    for (
+      let i = Math.max(1, pagination.currentPage - 2);
+      i <= Math.min(pagination.currentPage + 2, pagination.totalPages - 2);
+      i++
+    ) {
+      paginationLinks.items.push({
+        href: `/` + url + `?page=${i}`,
+        number: i + 1,
+        current: i === pagination.currentPage,
+      });
+    }
+
+    // Add an ellipsis before the last page if the 2nd last page is not in the window
+    if (pagination.currentPage < pagination.totalPages - 4) {
+      paginationLinks.items.push({ ellipsis: true });
+    }
+
+    // Add the last page if there is more than one page (don't repeat the first page)
+    if (pagination.totalPages > 1) {
+      paginationLinks.items.push({
+        href: `/` + url + `?page=${pagination.totalPages - 1}`,
+        number: pagination.totalPages,
+        current: pagination.totalPages - 1 === pagination.currentPage,
+      });
+    }
+
+    let title = pageTitle;
+    if (resourceLength > 0) {
+      title =
+        pageTitle +
+        ` ${pagination.currentPage * pagination.size + 1} to ${Math.min(
+          (pagination.currentPage + 1) * pagination.size,
+          pagination.totalElements
+        )} of ${pagination.totalElements}`;
+    }
+    return { paginationLinks, title };
+  }
+
   public async healthCheck(): Promise<void> {
     await axios.get('/health');
   }
