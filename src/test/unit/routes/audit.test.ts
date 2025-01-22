a3eace15a7cfbb5b61941a2278cc4261e94830dc
Phoebe Revolta <phoebe.revolta@solirius.com>
2025-01-22 09:37:59 +0000

    S28-3557: use mockRecordings as substitute for audit logs

--- src/test/unit/routes/audit.test.ts
+++ src/test/unit/routes/audit.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable jest/expect-expect */
 import { Nunjucks } from '../../../main/modules/nunjucks';
-import { mockGetAuditLogs, reset } from '../../mock-api';
+import { mockGetAuditLogs, mockRecordings, reset } from '../../mock-api';
 import { beforeAll, describe } from '@jest/globals';
 
 import { PreClient } from '../../../main/services/pre-api/pre-client';
@@ -67,132 +67,132 @@ describe('Audit route', () => {
     expect(response.status).toEqual(500);
   });
 
-  // test('pagination should have a previous link', async () => {
-  //   const app = require('express')();
-  //   new Nunjucks(false).enableFor(app);
-  //   const request = require('supertest');
-  //   const auditLogs = Array(12).fill(mockAuditLogs[0]).flat();
+  test('pagination should have a previous link', async () => {
+    const app = require('express')();
+    new Nunjucks(false).enableFor(app);
+    const request = require('supertest');
+    const auditLogs = Array(12).fill(mockRecordings[0]).flat();
 
-  //   mockGetAuditLogs(auditLogs, 1);
+    mockGetAuditLogs(auditLogs, 1);
 
-  //   const audit = require('../../../main/routes/audit').default;
-  //   audit(app);
+    const audit = require('../../../main/routes/audit').default;
+    audit(app);
 
-  //   const response = await request(app).get('/audit?page=1');
-  //   expect(response.status).toEqual(200);
-  //   const text = response.text.replace(/\s+/g, ' ').trim();
-  //   expect(text).toContain('<span class="govuk-pagination__link-title"> Previous');
-  // });
+    const response = await request(app).get('/audit?page=1');
+    expect(response.status).toEqual(200);
+    const text = response.text.replace(/\s+/g, ' ').trim();
+    expect(text).toContain('<span class="govuk-pagination__link-title"> Previous');
+  });
 
-  // test('pagination should have a next link', async () => {
-  //   const app = require('express')();
-  //   new Nunjucks(false).enableFor(app);
-  //   const request = require('supertest');
-  //   const auditLogs = Array(12).fill(mockAuditLogs[0]).flat();
+  test('pagination should have a next link', async () => {
+    const app = require('express')();
+    new Nunjucks(false).enableFor(app);
+    const request = require('supertest');
+    const auditLogs = Array(12).fill(mockRecordings[0]).flat();
 
-  //   mockGetAuditLogs(auditLogs, 0);
+    mockGetAuditLogs(auditLogs, 0);
 
-  //   const audit = require('../../../main/routes/audit').default;
-  //   audit(app);
+    const audit = require('../../../main/routes/audit').default;
+    audit(app);
 
-  //   const response = await request(app).get('/audit?page=0');
-  //   expect(response.status).toEqual(200);
-  //   const text = response.text.replace(/\s+/g, ' ').trim();
-  //   expect(text).toContain('<span class="govuk-pagination__link-title"> Next');
-  // });
+    const response = await request(app).get('/audit?page=0');
+    expect(response.status).toEqual(200);
+    const text = response.text.replace(/\s+/g, ' ').trim();
+    expect(text).toContain('<span class="govuk-pagination__link-title"> Next');
+  });
 
-  // test('pagination should have a filler ellipsis when more than 2 pages from the start', async () => {
-  //   const app = require('express')();
-  //   new Nunjucks(false).enableFor(app);
-  //   const request = require('supertest');
-  //   const auditLogs = Array(50).fill(mockAuditLogs[0]).flat();
+  test('pagination should have a filler ellipsis when more than 2 pages from the start', async () => {
+    const app = require('express')();
+    new Nunjucks(false).enableFor(app);
+    const request = require('supertest');
+    const auditLogs = Array(50).fill(mockRecordings[0]).flat();
 
-  //   mockGetAuditLogs(auditLogs, 4);
+    mockGetAuditLogs(auditLogs, 4);
 
-  //   const audit = require('../../../main/routes/audit').default;
-  //   audit(app);
+    const audit = require('../../../main/routes/audit').default;
+    audit(app);
 
-  //   const response = await request(app).get('/audit?page=4');
-  //   expect(response.status).toEqual(200);
-  //   const text = response.text.replace(/\s+/g, ' ').trim();
-  //   expect(text).toContain('<li class="govuk-pagination__item govuk-pagination__item--ellipses"> &ctdot; </li>');
-  // });
+    const response = await request(app).get('/audit?page=4');
+    expect(response.status).toEqual(200);
+    const text = response.text.replace(/\s+/g, ' ').trim();
+    expect(text).toContain('<li class="govuk-pagination__item govuk-pagination__item--ellipses"> &ctdot; </li>');
+  });
 
-  // test('pagination should have a filler ellipsis when more than 2 pages from the end', async () => {
-  //   const app = require('express')();
-  //   new Nunjucks(false).enableFor(app);
-  //   const request = require('supertest');
-  //   const auditLogs = Array(50).fill(mockAuditLogs[0]).flat();
+  test('pagination should have a filler ellipsis when more than 2 pages from the end', async () => {
+    const app = require('express')();
+    new Nunjucks(false).enableFor(app);
+    const request = require('supertest');
+    const auditLogs = Array(50).fill(mockRecordings[0]).flat();
 
-  //   mockGetAuditLogs(auditLogs, 0);
+    mockGetAuditLogs(auditLogs, 0);
 
-  //   const audit = require('../../../main/routes/audit').default;
-  //   audit(app);
+    const audit = require('../../../main/routes/audit').default;
+    audit(app);
 
-  //   const response = await request(app).get('/audit?page=0');
-  //   expect(response.status).toEqual(200);
-  //   const text = response.text.replace(/\s+/g, ' ').trim();
-  //   expect(text).toContain('<li class="govuk-pagination__item govuk-pagination__item--ellipses"> &ctdot; </li>');
-  // });
+    const response = await request(app).get('/audit?page=0');
+    expect(response.status).toEqual(200);
+    const text = response.text.replace(/\s+/g, ' ').trim();
+    expect(text).toContain('<li class="govuk-pagination__item govuk-pagination__item--ellipses"> &ctdot; </li>');
+  });
 
-  // test('pagination should show 2 pages either side of the current page', async () => {
-  //   const app = require('express')();
-  //   new Nunjucks(false).enableFor(app);
-  //   const request = require('supertest');
-  //   const auditLogs = Array(90).fill(mockAuditLogs[0]).flat();
+  test('pagination should show 2 pages either side of the current page', async () => {
+    const app = require('express')();
+    new Nunjucks(false).enableFor(app);
+    const request = require('supertest');
+    const auditLogs = Array(90).fill(mockRecordings[0]).flat();
 
-  //   mockGetAuditLogs(auditLogs, 4);
+    mockGetAuditLogs(auditLogs, 4);
 
-  //   const audit = require('../../../main/routes/audit').default;
-  //   audit(app);
+    const audit = require('../../../main/routes/audit').default;
+    audit(app);
 
-  //   const response = await request(app).get('/audit?page=4');
-  //   const text = response.text.replace(/\s+/g, ' ').trim();
-  //   expect(response.status).toEqual(200);
-  //   expect(text).toContain('> 1 <');
-  //   expect(text).toContain('<li class="govuk-pagination__item govuk-pagination__item--ellipses"> &ctdot; </li>');
-  //   expect(text).toContain('> 3 <');
-  //   expect(text).toContain('> 4 <');
-  //   expect(text).toContain('> 5 <');
-  //   expect(text).toContain('> 6 <');
-  //   expect(text).toContain('> 7 <');
-  //   expect(text).toContain('> 9 <');
-  // });
+    const response = await request(app).get('/audit?page=4');
+    const text = response.text.replace(/\s+/g, ' ').trim();
+    expect(response.status).toEqual(200);
+    expect(text).toContain('> 1 <');
+    expect(text).toContain('<li class="govuk-pagination__item govuk-pagination__item--ellipses"> &ctdot; </li>');
+    expect(text).toContain('> 3 <');
+    expect(text).toContain('> 4 <');
+    expect(text).toContain('> 5 <');
+    expect(text).toContain('> 6 <');
+    expect(text).toContain('> 7 <');
+    expect(text).toContain('> 9 <');
+  });
 
-  // test('pagination should show all the page numbers', async () => {
-  //   const app = require('express')();
-  //   new Nunjucks(false).enableFor(app);
-  //   const request = require('supertest');
-  //   const auditLogs = Array(40).fill(mockAuditLogs[0]).flat();
+  test('pagination should show all the page numbers', async () => {
+    const app = require('express')();
+    new Nunjucks(false).enableFor(app);
+    const request = require('supertest');
+    const auditLogs = Array(40).fill(mockRecordings[0]).flat();
 
-  //   mockGetAuditLogs(auditLogs, 1);
+    mockGetAuditLogs(auditLogs, 1);
 
-  //   const audit = require('../../../main/routes/audit').default;
-  //   audit(app);
+    const audit = require('../../../main/routes/audit').default;
+    audit(app);
 
-  //   const response = await request(app).get('/audit?page=1');
-  //   const text = response.text.replace(/\s+/g, ' ').trim();
-  //   expect(response.status).toEqual(200);
-  //   expect(text).toContain('> 1 <');
-  //   expect(text).toContain('> 2 <');
-  //   expect(text).toContain('> 3 <');
-  //   expect(text).toContain('> 4 <');
-  //   expect(text).not.toContain('<li class="govuk-pagination__item govuk-pagination__item--ellipses"> &ctdot; </li>');
-  // });
+    const response = await request(app).get('/audit?page=1');
+    const text = response.text.replace(/\s+/g, ' ').trim();
+    expect(response.status).toEqual(200);
+    expect(text).toContain('> 1 <');
+    expect(text).toContain('> 2 <');
+    expect(text).toContain('> 3 <');
+    expect(text).toContain('> 4 <');
+    expect(text).not.toContain('<li class="govuk-pagination__item govuk-pagination__item--ellipses"> &ctdot; </li>');
+  });
 
-  // test('heading should contain current page, max page and number of auditLogs', async () => {
-  //   const app = require('express')();
-  //   new Nunjucks(false).enableFor(app);
-  //   const request = require('supertest');
-  //   const auditLogs = Array(100).fill(mockAuditLogs[0]).flat();
+  test('heading should contain current page, max page and number of auditLogs', async () => {
+    const app = require('express')();
+    new Nunjucks(false).enableFor(app);
+    const request = require('supertest');
+    const auditLogs = Array(100).fill(mockRecordings[0]).flat();
 
-  //   mockGetAuditLogs(auditLogs, 4);
+    mockGetAuditLogs(auditLogs, 4);
 
-  //   const audit = require('../../../main/routes/audit').default;
-  //   audit(app);
+    const audit = require('../../../main/routes/audit').default;
+    audit(app);
 
-  //   const response = await request(app).get('/audit?page=4');
-  //   expect(response.status).toEqual(200);
-  //   expect(response.text).toContain('AuditLogs 41 to 50 of 100');
-  // });
+    const response = await request(app).get('/audit?page=4');
+    expect(response.status).toEqual(200);
+    expect(response.text).toContain('Audit Logs 41 to 50 of 100');
+  });
 });
