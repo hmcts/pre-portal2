a24ae26354ed6ea73951ac6fc6541714dd8475d8
Phoebe Revolta <phoebe.revolta@solirius.com>
2025-01-23 10:52:38 +0000

    S28-3557: remove super-user check as this is checked for in getAuditLogs, the catch will be triggered if this is unsuccessful

--- src/main/routes/audit.ts
+++ src/main/routes/audit.ts
@@ -5,40 +5,31 @@ import { SessionUser } from '../services/session-user/session-user';
 import { Application } from 'express';
 import { requiresAuth } from 'express-openid-connect';
 import { UserLevel } from '../types/user-level';
-import { NotFoundError } from '../types/errors';
 
 export default function (app: Application): void {
   app.get('/audit', requiresAuth(), async (req, res) => {
     const client = new PreClient();
 
-    const isSuperUser =
-      SessionUser.getLoggedInUserProfile(req).app_access.filter(role => role.role.name === UserLevel.SUPER_USER)
-        .length > 0;
-
     try {
-      if (isSuperUser) {
-        const request: SearchAuditLogsRequest = {
-          page: req.query.page as unknown as number,
-          size: 10,
-        };
+      const request: SearchAuditLogsRequest = {
+        page: req.query.page as unknown as number,
+        size: 10,
+      };
 
-        const { auditLogs, pagination } = await client.getAuditLogs(
-          await SessionUser.getLoggedInUserProfile(req).app_access.filter(
-            role => role.role.name === UserLevel.SUPER_USER
-          )?.[0].id,
-          request
-        );
+      const { auditLogs, pagination } = await client.getAuditLogs(
+        await SessionUser.getLoggedInUserProfile(req).app_access.filter(
+          role => role.role.name === UserLevel.SUPER_USER
+        )?.[0].id,
+        request
+      );
 
-        const { paginationLinks, title } = client.createPagination(pagination, 'audit', 'Audit Logs', auditLogs.length);
+      const { paginationLinks, title } = client.createPagination(pagination, 'audit', 'Audit Logs', auditLogs.length);
 
-        res.render('audit', {
-          auditLogs,
-          paginationLinks,
-          title,
-        });
-      } else {
-        throw new NotFoundError();
-      }
+      res.render('audit', {
+        auditLogs,
+        paginationLinks,
+        title,
+      });
     } catch (err) {
       res.status(404);
       res.render('not-found');
