b209f34d8db989295f18d9b6e07107ace2b65103
amandarichards <Amanda.Richards@HMCTS.NET>
2025-02-25 14:40:24 +0000

    switch to goto

--- src/main/services/system-status/live-events-status.ts
+++ src/main/services/system-status/live-events-status.ts
@@ -1,43 +1,3 @@
-// import { PreClient } from '../pre-api/pre-client';
-// import { LiveEvent } from '../../types/live-event';
-// import { SessionUser } from '../session-user/session-user';
-// import { UserLevel } from '../../types/user-level';
-// import { Request } from 'express';
-//
-// export class LiveEventStatusService {
-//     private client: PreClient;
-//     private user: string | undefined;
-//
-//
-//     constructor(req: Request, client: PreClient) {
-//         this.client = client;
-//         const loggedInUser = SessionUser.getLoggedInUserProfile(req);
-//         this.user = loggedInUser.app_access.find(role => role.role.name === UserLevel.SUPER_USER)?.id
-//     }
-//
-//     public async getMediaKindLiveEventStatuses() {
-//         try {
-//           if (!this.user) {
-//                         throw new Error('User not authorized to access live events.');
-//                     }
-//             const liveEvents: LiveEvent[] = await this.client.getLiveEvents(this.user);
-//         if (liveEvents.length > 0) {
-//             return liveEvents.map(event => ({
-//                 id: event.id || "Unknown ID",
-//                 name: event.name || 'Unknown Name',
-//                 description: event.description || 'Unknown Description',
-//                 status: event.resource_state || 'Unknown Status'
-//
-//             }))
-//           else {
-//           console.log('no live events');
-//         }} catch (error) {
-//             console.error('Error fetching live events:', error);
-//             throw new Error('Failed to retrieve live event statuses.');
-//         }}
-//     }
-// }
-
 import { PreClient } from '../pre-api/pre-client';
 import { LiveEvent } from '../../types/live-event';
 import { SessionUser } from '../session-user/session-user';
@@ -66,8 +26,7 @@ export class LiveEventStatusService {
       const liveEvents: LiveEvent[] = await this.client.getLiveEvents(this.user);
 
       if (liveEvents.length === 0) {
-        //console.log('No live events found.');
-        return []; // Return an empty array instead of continuing to map
+        return [];
       }
 
       return liveEvents.map(event => ({
@@ -77,7 +36,6 @@ export class LiveEventStatusService {
         status: event.resource_state || 'Unknown Status',
       }));
     } catch (error) {
-      //console.error('Error fetching live events:', error);
       throw new Error('Failed to retrieve live event statuses.');
     }
   }
