e87a82d3dbff0fc864f0f7836a3e8e077615b101
Josh Blackmoor <joshblackmoor01@icloud.com>
2025-03-05 13:23:56 +0000

    lint

--- src/test/unit/routes/help.ts
+++ src/test/unit/routes/help.ts
@@ -13,9 +13,12 @@ describe('Help page', () => {
     const response = await request(app).get('/help');
     expect(response.status).toBe(200);
     expect(response.text).toContain('Help');
-    expect(response.text).toContain('Please follow instructions in the user guide provided ' +
-      'in the Portal invitation email.')
-    expect(response.text).toContain('If you require further assistance, please contact support on ' +
-      '0300 323 0194 between the hours of 08:00 and 18:00 weekdays, or 08:30 and 14:00 Saturday.')
+    expect(response.text).toContain(
+      'Please follow instructions in the user guide provided ' + 'in the Portal invitation email.'
+    );
+    expect(response.text).toContain(
+      'If you require further assistance, please contact support on ' +
+        '0300 323 0194 between the hours of 08:00 and 18:00 weekdays, or 08:30 and 14:00 Saturday.'
+    );
   });
 });
